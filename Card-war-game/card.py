fe8848c5551e8a75ed262095e216450a357b5cf9
Regidan <regidancodes@gmail.com>
2024-01-19 19:29:04 +0530

    Added deck.py

--- Card-war-game/card.py
+++ Card-war-game/card.py
@@ -16,7 +16,13 @@ class Card:
     def show(self):
         card_value = self._value
         card_suit = self._suit
-        suit_symbol = self._suit._description.capitalize()
+        suit_symbol = self._suit.description.capitalize()
+
+        if self.is_special():
+            card_description = Card.SPECIAL_CARDS[card_value]
+            print(f"{card_description} of {card_suit} {suit_symbol}")
+        else:
+            print(f"{card_value} of {card_suit} {suit_symbol}")
 
     def is_special(self):
         return self._value >= 11
