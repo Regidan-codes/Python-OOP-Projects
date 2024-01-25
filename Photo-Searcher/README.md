08f461284151961579ba4018a9eb1ce62ed40982
Regidan <regidancodes@gmail.com>
2024-01-26 01:16:48 +0530

    done

--- Photo-Searcher/README.md
+++ Photo-Searcher/README.md
@@ -1,1 +1,5 @@
 # What is the project about?
+
+Image search using kivy. Image of UI below:
+
+![kivy.png](kivy.png)
--- Photo-Searcher/main.py
+++ Photo-Searcher/main.py
@@ -1,6 +1,8 @@
 from kivy.app import App
 from kivy.uix.screenmanager import ScreenManager, Screen
 from kivy.lang import Builder
+import wikipedia
+import requests
 
 Builder.load_file('frontend.kv')
 
@@ -8,7 +10,31 @@ Builder.load_file('frontend.kv')
 class FirstScreen(Screen):
 
     def search_image(self):
-        pass
+        query = self.manager.current_screen.ids.user_query.text
+        try:
+            page = wikipedia.page(title=query)
+
+            wiki_image = page.images[0]
+
+            request = requests.get(wiki_image)
+
+            if request.status_code == 403:
+                headers = {
+                    'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, '
+                                  'like Gecko) Chrome/39.0.2171.95 Safari/537.36'}
+                request = requests.get(wiki_image, headers=headers)
+
+            content = request.content
+            image_path = 'files/result.jpg'
+
+            with open(image_path, 'wb') as output:
+                output.write(content)
+
+            self.manager.current_screen.ids.img.source = image_path
+
+        except:
+
+            print('Ambiguous title')
 
 
 class RootWidget(ScreenManager):
